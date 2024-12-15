4a5e2f7b25050475900edff2dc989a4f48be8b6b
Atharva Kori <atharva.kori23@pccoepune.org>
2024-12-15 12:46:53 +0530

    Update Remove_Duplicates_from_Sorted_Array.java
    Provided one more method

--- Remove_Duplicates_from_Sorted_Array.java
+++ Remove_Duplicates_from_Sorted_Array.java
@@ -1,3 +1,5 @@
+<H1>Method 1</H1>
+
 class Solution {
     public int removeDuplicates(int[] nums) {
         int index = 0;
@@ -9,5 +11,27 @@ class Solution {
             }
         }
         return index;
+    }
+}
+
+----------------------------------
+<H1>Method 2</H1>
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        Set<Integer> uniqueNums = new HashSet<>();
+        for(int num : nums){
+            uniqueNums.add(num);
+        }
+        int result[] = new int[uniqueNums.size()];
+        int index = 0;
+        for(int num : uniqueNums){
+            result[index++] = num;
+        }
+        int i = 0;
+        for(int num : result){
+            nums[i++] = num;
+        }
+        return uniqueNums.size();
     }
 }
